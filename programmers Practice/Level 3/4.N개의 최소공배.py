4fcc7109c06e7c5baf6f5f07d739f5760f730829
quan <mearisori@gmail.com>
2018-01-26 13:50:17 +0800

    좀 길게 썼음... 음 ... 아주 길게

--- programmers Practice/Level 3/4.N개의 최소공배.py
+++ programmers Practice/Level 3/4.N개의 최소공배.py
@@ -8,30 +8,98 @@
 # 최소공배수를 반환해 주세요. 예를들어 [2,6,8,14] 가 입력된다면 168을 반환해 주면 됩니다.
 
 
-
-def check_prime(x):
-    for i in range(2, x):
-        if x % i == 0:
-            return False
-    return True
-
-
-b = []
-
-
-def disolve(a):
-    while check_prime(a) == False:
-
-        for i in range(2, a):
-            if a % i == 0:
-                a = a // i
-                b.append(i)
-    return b
+# import random
+# import math
+#
+# a = [random.randrange(50, 150) for i in range(5)]
+# print(a)
+#
+# b = random.randrange(100)
+# print(b)
+#
+import math
+from functools import reduce
+import random
 
 
-print(disolve(500))
+def soyinsu(n):
+    # n 은  정수 타입이다
+    k = []
+    i = 2
+    while i < (int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            k.append(i)
+            n = n / i
+        else:
+            i += 1
+    k.append(int(n))
+    return k
+
+
+def lcm_two(a, b):
+    # a,b type is list
+    c = []
+    d = a + b
+    if len(a) < len(b):
+        a, b = b, a
+
+    for i in set(d):
+
+        if a.count(i) >= b.count(i):
+            c.extend([i for x in range(a.count(i))])
+        else:
+            c.extend([i for x in range(b.count(i))])
+
+    # print(c, 'c')
+    lcm = reduce(lambda x, y: x * y, c)
+    return lcm
+
+
+# print(lcm_two([2,3],[3]))
+# print(lcm_two([3],[2,3]))
+def lcm(cc):
+    i = 1
+    x = cc[0]
+    while i < len(cc):
+        x = lcm_two(soyinsu(x), soyinsu(cc[i]))
+        i += 1
+
+    return x
+
+
+print(lcm([2, 6, 8, 14]))
+# print(x, 'x')
+# print(lcm_two(soyinsu(24), soyinsu(14)), '24,14')
+# print(soyinsu(24), soyinsu(14))
 
 #
+# print(lcm([2, 6, 8, 14]))
+#
+# print(lcm_two(soyinsu(2), soyinsu(6)), soyinsu(2), soyinsu(6))
+
+# test if soyinsu() is OK.
+# n = (random.randrange(1, 10000000000) for i in range(1, 10))
+# for i in n:
+#     if i == reduce(lambda x, y: x * y, soyinsu(i)):
+#         print(i, soyinsu(i), True)
+#         print(soyinsu(soyinsu(i)[-1]) if len(soyinsu(soyinsu(i)[-1]))==1
+#               else 'Oh,it\'s a sad thing that you have wrong answer' )
+#     else:
+#         print('Oh,it\'s a sad thing that you have wrong answer')
+
+# print(n)
+# nn = soyinsu(n)
+#
+# print(soyinsu(n))
+# print(reduce(lambda x, y: x * y, nn))
+
+# [2, 2, 2, 3, 3]
+# [2, 2, 2, 2, 2, 2]
+# [2, 2, 2, 3]
+
+# y = k.append(int(n))
+# print(y)
+# #
 # def nlcm(num):
 #     answer = 0
 #
