ac71993dfef7f782845629325a23efb489f16e0f
quan <mearisori@gmail.com>
2018-02-03 00:02:54 +0800

    update

--- CheckIO/Electronic Station/1.brackets.py
+++ CheckIO/Electronic Station/1.brackets.py
@@ -1,25 +1,42 @@
-import
 def checkio(e):
-    e.
+    li = []
+    element_left = ['(', '{', '[']
+    element_right = [')', '}', ']']
+    elements_zip = zip(element_right,element_left)
+    elements = element_left + element_right
+    for i in e:
+        if i in elements:
+            li.append(i)
+        elif i in element_right:
+            pass
+
+    # if len(li) % 2 and len(li) > 0:
+    #     return False
+
+    return li
 
 
+print(checkio("((5+3)*2+1)"))
+print(checkio("{[(3+1)+2]+}"))
+print(checkio("(3+{1-1)}"))
+print(checkio("(({[(((1)-2)+3)-3]/3}-3)"))
+print(checkio("[1+1]+(2*2)-{3/3}"))
 
-
-# These "asserts" using only for self-checking and not necessary for auto-testing
-
-if __name__ == '__main__':
-    assert checkio("((5+3)*2+1)") == True, "Simple"
-    assert checkio("{[(3+1)+2]+}") == True, "Different types"
-    assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
-    assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
-    assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
-    assert checkio("2+3") == True, "No brackets, no problem"
-
+# # These "asserts" using only for self-checking and not necessary for auto-testing
 #
+# if __name__ == '__main__':
+#     assert checkio("((5+3)*2+1)") == True, "Simple"
+#     assert checkio("{[(3+1)+2]+}") == True, "Different types"
+#     assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
+#     assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
+#     assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
+#     assert checkio("2+3") == True, "No brackets, no problem"
+
 # You are given an expression with numbers, brackets and operators.
 # For this task only the brackets matter. Brackets come in three flavors:
-#  "{}" "()" or "[]". Brackets are used to determine scope or to restrict some expression.
-# If a bracket is open, then it must be closed with a closing bracket of the same type .# The scope of a bracket must not intersected by another bracket.
+# "{}" "()" or "[]". Brackets are used to determine scope or to restrict some expression.
+# If a bracket is open, then it must be closed with a closing bracket of the same type .
+# The scope of a bracket must not intersected by another bracket.
 
 
 # In this task you should make a decision,
--- CheckIO/Electronic Station/5.roman numerals.py
+++ CheckIO/Electronic Station/5.roman numerals.py
@@ -1,38 +1,90 @@
-a = 499
+def make_roman(one, five, ten):
+    li = []
+    if five and ten:
+        for i in range(0, 12):
+            if i == 1:
+                li.append(one)
+            elif i > 1 and i < 4:
+                li.append(one * i)
+            elif i == 4:
+                li.append(one + five)
+            elif i == 5:
+                li.append(five)
+            elif i > 5 and i < 9:
+                li.append(five + (one * (i - 5)))
+            elif i == 9:
+                li.append(one + ten)
+            elif i == 10:
+                li.append(ten)
+            elif i == 11:
+                li.append('')
+    else:
+        for i in range(1, 4):
+            if i == 1:
+                li.append(one)
+            elif i > 1 and i < 4:
+                li.append(one * i)
+    return li
 
 
-def checkio(a):
-    k = [1, 5, 10, 50, 100, 500, 1000]
-
-    data = []
-
-    def check(a):
-        for i in range(len(k)):
-            if a >= 1000:
-                return 1000
-            if k[i] < a < k[i + 1]:
-                return k[i]
-
-    while a > 0:
-        data.append((a // check(a), check(a)))
-        a = a % check(a)
-
-    return data
+one = make_roman("I", "V", "X")
+two = make_roman("X", "L", "C")
+three = make_roman("C", "D", "M")
+four = make_roman("M", None, None)
+roman = [one, two, three, four]
 
 
-print(checkio(a))
+def checkio(data):
+    x = []
+
+    data = list(str(data))
+    data.reverse()
+    for i in range(len(data)):
+        x.append(roman[i][int(data[i]) - 1])
+
+    x.reverse()
+    x = ''.join(x)
+    return x
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
+    print("OK?Let us check it out")
+# 罗马数字来源于古罗马编码系统。它们是基于字母表的特定字母的组合，
+# 所表示的数等于这些数字相加（或者是相减）得到的数。前十位的罗马数字是：
 #
-# if __name__ == '__main__':
-#     # These "asserts" using only for self-checking and not necessary for auto-testing
-#     # assert checkio(6) == 'VI', '6'
-#     assert checkio(76) == 'LXXVI', '76'
-#     assert checkio(499) == 'CDXCIX', '499'
-#     assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
-
-# I	1 (unus)
-# V	5 (quinque)
-# X	10 (decem)
-# L	50 (quinquaginta)
-# C	100 (centum)
-# D	500 (quingenti)
-# M	1000 (mille)
+# I，II，III，IV，V，VI，VII，VIII，IX和X。
+#
+# 罗马记数系统不是直接的十进制为基础，它没有零。
+# 罗马数字是根据这七个符号的组合：
+#
+# 符号
+# 值
+# I
+# 1(unus)
+# V
+# 5(quinque)
+# X
+# 10(decem)
+# L
+# 50(quinquaginta)
+# C
+# 100(centum)
+# D
+# 500(quingenti)
+# M
+# 1, 000(mille)
+# 更多额外的关于罗马数字的信息可以参考
+# 维基百科的文章.
+#
+# 在这个任务里，你应该返回给出指定的整数值的范围从1到3999的罗马数字。
+#
+# 输入: 一个整数(int).
+#
+# 输出: 一个字符串形式的罗马数字(str).
+#
+# 前提: 0 < number < 4000
