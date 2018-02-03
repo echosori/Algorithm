6710391c53dabfa569a61217625aa6c02c0cbd31
quan <mearisori@gmail.com>
2018-02-03 13:07:50 +0800

    업데이트
    업데이트

--- TEST.py
+++ TEST.py
@@ -66,25 +66,23 @@
 #
 # 前提: 所有的测试用例都是正确的并且函数不会引发异常。
 
-def min(*args, **kwargs):
-    key = kwargs.get("key", None)
-    while i <= len(args):
 
-    return None
-
-#
-# def max(*args, **kwargs):
-#     key = kwargs.get("key", None)
-#     return None
+# ----------------------------------------------------------------------------------------
+import datetime
 
 
+def days_diff(date1, date2):
+    """
+        Find absolute diff in days between dates
+    """
+    return abs((datetime.datetime(*date1) - datetime.datetime(*date2)).days)
+
+
+print(days_diff((1982, 4, 19), (1982, 4, 22)))
+print(days_diff((2014, 1, 1), (2014, 8, 27)))
+print(days_diff((2014, 8, 27), (2014, 1, 1)))
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert max(3, 2) == 3, "Simple case max"
-    assert min(3, 2) == 2, "Simple case min"
-    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
-    assert min("hello") == "e", "From string"
-    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
-    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
-
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
